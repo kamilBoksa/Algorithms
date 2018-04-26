7e25aa15f00e524969708d38ce56bb15329bbad9
F4tall <kamil.boksa@gmail.com>
2018-04-26 21:25:54 +0200

    Add PrimeFactorization algorithm

---
+++ PrimeFactorization/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PrimeFactors primeFactorization = new PrimeFactors();
+        primeFactorization.displayPrimeFactors(args[0]);
+    }
+}
---
+++ PrimeFactorization/src/PrimeFactors.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+
+    private static List<Integer> getPrimeFactors(int number) {
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= number / i; i++) {
+            while (number % i == 0) {
+                factors.add(i);
+                number /= i;
+            }
+        }
+        if (number > 1) {
+            factors.add(number);
+        }
+        return factors;
+    }
+
+    public void displayPrimeFactors(String arg) {
+        System.out.println("Primefactors of "+ arg);
+        for (Integer integer : getPrimeFactors(Integer.parseInt(arg))) {
+            System.out.println(integer);
+        }
+    }
+}
